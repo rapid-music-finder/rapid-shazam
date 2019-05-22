209e8d3274f4c7b54048349bf518d40fae34bdc0
Ania Nakayama and shruti <ania.jurago+shrutiitengg@gmail.com>
2019-05-22 17:51:46 +0900

    resolved merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -6,58 +6,34 @@
         <span class="font-weight-light">Shazam</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-<<<<<<< HEAD
-      <v-btn
-        flat
-        href="https://github.com/rapid-music-finder/rapid-shazam"
-        target="_blank"
-      >
-        <span class="mr-2">Github</span>
-=======
       <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
->>>>>>> results_page
       </v-btn>
     </v-toolbar>
 
     <v-content>
-<<<<<<< HEAD
       <Search/>
-=======
       <Results/>
->>>>>>> results_page
     </v-content>
   </v-app>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Search from './components/Search'
-=======
+import Search from "./components/Search";
 import Results from "./components/Results";
->>>>>>> results_page
 
 export default {
   name: "App",
   components: {
-<<<<<<< HEAD
-    Search
-=======
+    Search,
     Results
->>>>>>> results_page
   },
   data() {
     return {
       //
-<<<<<<< HEAD
-    }
-  },
-}
-=======
     };
   }
 };
->>>>>>> results_page
 </script>
 
 <style>
