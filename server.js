427573816d17dd52e0b19d459881576734445b8e
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-05-22 14:28:22 +0900

    created env-files

--- server.js
+++ server.js
@@ -2,6 +2,28 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+const axios = require('axios');
+require('dotenv').config();
+
+const MUSIC_GRAPH_KEY = process.env.MUSIC_GRAPH_KEY;
+const MUSIC_GRAPH_ID = process.env.MUSIC_GRAPH_ID;
+const X_RAPIDAPI_KEY = process.env.X_RAPIDAPI_KEY;
+
+app.get('/api/songs/:title', (req, res, next) => {
+  const { title } = req.params
+  const data = { key: MUSIC_GRAPH_KEY, id: MUSIC_GRAPH_ID, data: {search: title} };
+  const headers = { 
+      "X-RapidAPI-Host": "macgyverapi-music-graph-v1.p.rapidapi.com",
+      "Content-Type": "application/json",
+      "X-RapidAPI-Key": X_RAPIDAPI_KEY
+    };
+  
+  axios.post("https://macgyverapi-music-graph-v1.p.rapidapi.com/", data, {headers: headers})
+    .then(result => {
+      res.send(result.data.result);
+    })
+})
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 8000;
 app.listen(port);
